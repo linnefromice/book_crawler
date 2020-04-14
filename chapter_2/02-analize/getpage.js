ac90dbaacd89dc6ca0ba8ba3c3997e15e383ca6e
linnefromice <linnefromice@gmail.com>
2020-04-14 23:08:49 +0900

    create for html file download

--- chapter_2/02-analize/getpage.js
+++ chapter_2/02-analize/getpage.js
@@ -1,3 +1,17 @@
 const client = require('cheerio-httpcli');
 
-//TODO: logic
+const url = "https://www.aozora.gr.jp/index_pages/person81.html";
+let param = {};
+
+const main = () => {
+    client.fetch(url, param, (err, $, res) => {
+        if (err) {
+            console.log("Error:", err);
+            return;
+        }
+        let body = $.html();
+        console.log(body);
+    });
+}
+
+main();
