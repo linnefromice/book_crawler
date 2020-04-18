78b94501b6d9b42cf114933aa24e685ed0e34238
linnefromice <linnefromice@gmail.com>
2020-04-18 23:57:16 +0900

    modify logic file for 3-1

--- chapter_3/01-js/getTitle.js
+++ chapter_3/01-js/getTitle.js
@@ -7,8 +7,12 @@ const getTitle = async () => {
   });
   const page = await browser.newPage();
   await page.goto('https://www.google.com/')
-  const title = await page.title();
+  let title = await page.title();
   console.log(title);
+  await page.goto('https://www.facebook.com/')
+  console.log(await page.title());
+  await page.goto('https://www.microsoft.com/')
+  console.log(await page.title());
   await browser.close();
 }
 
