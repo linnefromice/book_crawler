cdd2cd9b79ca3d23432fea07471c9e3f19ce61d7
linnefromice <linnefromice@gmail.com>
2020-04-18 22:00:34 +0900

    modify logic file for puppeteer trial

--- self-study/puppeteer-practice/example.js
+++ self-study/puppeteer-practice/example.js
@@ -9,15 +9,24 @@ const operate_browser_one = async () => {
   await page.goto('https://www.google.com/')
   await page.type('input[name=q]', 'こんにちは', { delay: 50 }) 
   await page.click('input[type="submit"]')
-  /*
-  await page.goto('https://example.com/');
-  console.log(page.title);
-  */
   await browser.close();
 }
 
-const main = () => {
-  operate_browser_one();
+const operate_browser_two = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 300 
+  });
+  const page = await browser.newPage();
+  await page.goto('https://example.com/');
+  const title = await page.title();
+  console.log(title);
+  await browser.close();
+}
+
+const main = async () => {
+  await operate_browser_one();
+  await operate_browser_two();
 }
 
 main();
