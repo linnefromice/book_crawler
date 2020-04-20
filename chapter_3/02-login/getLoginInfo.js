9a75b62d385c6108f81c2102770519ea53d6a7e7
linnefromice <linnefromice@gmail.com>
2020-04-20 15:15:57 +0900

    create logic file for 3-2 - move from login page to home page

--- chapter_3/02-login/getLoginInfo.js
+++ chapter_3/02-login/getLoginInfo.js
@@ -6,12 +6,19 @@ const launchTypingClub = async () => {
     // console.log(loginInfo.typing_club.password);
     const browser = await puppeteer.launch({
         headless: false,
-        slowMo: 300,
+        slowMo: 100,
     });
     const page = await browser.newPage();
     await page.goto('https://www.typingclub.com/')
     await page.click('.login-link');
-    await page.waitFor(2000);
+    await page.waitForNavigation();
+    await page.waitFor(3000); // provisional
+    await page.type('.form-group > #username', loginInfo.typing_club.username);
+    await page.type('.form-group > #password', loginInfo.typing_club.password);
+    await page.click('#login-with-password');
+    await page.waitForNavigation();
+    await page.waitFor(3000); // provisional
+    await page.screenshot({path: 'screenShotPage.png'});
     await browser.close();
 }
 
