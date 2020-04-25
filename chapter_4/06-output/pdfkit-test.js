082bb8b8a5f30cbc8505b1c8c01f881235efd704
linnefromice <linnefromice@gmail.com>
2020-04-25 19:08:52 +0900

    update logic file for 4-6

--- chapter_4/06-output/pdfkit-test.js
+++ chapter_4/06-output/pdfkit-test.js
@@ -11,6 +11,9 @@ const main = () => {
     doc.pipe(fs.createWriteStream('sample.pdf'))
     doc.fontSize(30).text('Warren Edward Buffett', 90, 100)
     doc.fontSize(20).text(content, 100, 180)
+    doc.save().moveTo(80, 80).lineTo(400, 80).lineTo(400, 150).lineTo(80, 150).lineTo(80, 80).stroke('#0000FF');
+    doc.addPage();
+    doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill('FFAAAA')
     doc.end()
 }
 
